d3653d012699d1a63d8b724d5b3cef8664cc2727
tdubreucq <thibaud.dubreucq@gmail.com>
2020-06-28 19:25:02 +0200

    Rerouting update related to the production

--- server.ts
+++ server.ts
@@ -1,7 +1,16 @@
 import express, { Application} from 'express';
 import indexRoutes from './routes/index';
 import cors from 'cors'
+import * as path from "path";
 const app: Application = express();
+
+if (process.env.MODE === 'PROD') {
+    app.use(express.static(path.join(__dirname, 'build')));
+
+    app.get('/*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
+}
 
 // middlewares
 app.use(express.json());
